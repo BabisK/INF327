c33ef220bdfd922a4203c847411dc41257f95f35
Babis Kaidos <babis_k@outlook.com>
2017-02-20 17:26:01 +0200

    Changes in code

--- timeseries.r
+++ timeseries.r
@@ -11,7 +11,7 @@ library("forecast")
 library("fGarch")
 
 # Load and name the data
-header <- c("HFRI ", "EH", "M", "RVA", "ED", "CA", "DS", "EMN", "MA", "RUS_Rf", "RUS_1_Rf_1", "MXUS_Rf", "MEM_Rf", "SMB", "HML", "MOM", "SBGC_Rf", "SBWG_Rf", "LHY_Rf", "DEFSPR", "FRBI_Rf", "GSCI__Rf", "VIX", "Rf")
+header <- c("HFRI", "EH", "M", "RVA", "ED", "CA", "DS", "EMN", "MA", "RUS_Rf", "RUS_1_Rf_1", "MXUS_Rf", "MEM_Rf", "SMB", "HML", "MOM", "SBGC_Rf", "SBWG_Rf", "LHY_Rf", "DEFSPR", "FRBI_Rf", "GSCI__Rf", "VIX", "Rf")
 dates = seq(from = as.Date("1990-04-01", format='%Y-%m-%d'), to = as.Date("2005-12-01", format='%Y-%m-%d'), by = 'month')
 as_data <- read.table("data_assignment.txt")
 colnames(as_data) <- header
@@ -20,22 +20,24 @@ rownames(as_data) <- dates
 # Plot all time series
 png(filename = "images/dependentVariables.png", width = 2400, height = 2000, res = 250)
 par(mfrow=c(4,2))
-for (col in c("HFRI ", "EH", "M", "RVA", "ED", "CA", "DS", "EMN")){
+for (col in c("HFRI", "EH", "M", "RVA", "ED", "CA", "DS", "EMN")){
   plot(ts(data = as_data[[paste(col)]], start = c(1990, 4), end = c(2004, 12), frequency = 12), ylab = paste(col))
 }
 par(mfrow=c(1,1))
 dev.off()
 
 # Analyze HFRI
-par(mfrow = c(3,1))
 HFRI_data <- ts(data = as_data$HFRI, start = c(1990, 4), end = c(2004, 12), frequency = 12)
-plot(HFRI_data)
 ar <- ar(HFRI_data)
-adfTest(HFRI_data, lags = ar$order, type = "ct")
+adfTest(HFRI_data, lags = ar$order, type = "c")
 
+png(filename = "images/HFRIexploration.png", width = 2400, height = 2000, res = 250)
+par(mfrow = c(3,1))
+plot(HFRI_data)
 acf(HFRI_data)
 pacf(HFRI_data)
 par(mfrow = c(1,1))
+dev.off()
 
 boxPierce <- Box.test(HFRI_data,1,type="Box-Pierce")
 boxLjung <- Box.test(HFRI_data,1,type="Ljung-Box")
@@ -43,15 +45,17 @@ boxLjung <- Box.test(HFRI_data,1,type="Ljung-Box")
 modelHFRI = arima(HFRI_data, order = c(0,0,1))
 
 # Analyze DS
-par(mfrow = c(3,1))
 DS_data <- ts(data = as_data$DS, start = c(1990, 4), end = c(2004, 12), frequency = 12)
-plot(DS_data)
 ar <- ar(DS_data)
-adfTest(DS_data, lags = ar$order, type = "ct")
+adfTest(DS_data, lags = ar$order, type = "c")
 
+png(filename = "images/DSexploration.png", width = 2400, height = 2000, res = 250)
+par(mfrow = c(3,1))
+plot(DS_data)
 acf(DS_data)
 pacf(DS_data)
 par(mfrow = c(1,1))
+dev.off()
 
 boxPierce <- Box.test(DS_data,2,type="Box-Pierce")
 boxLjung <- Box.test(DS_data,2,type="Ljung-Box")
